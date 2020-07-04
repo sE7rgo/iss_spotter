60729a82ed09155b10861e3c74e91b289b614e12
sE7rgo <se7rgo@gmail.com>
2020-07-04 02:32:08 +0000

    added promises, clean and simple

---
+++ iss_promised.js
@@ -0,0 +1,28 @@
+const request = require('request-promise-native');
+
+const fetchMyIP = function() {
+  return request('https://api.ipify.org?format=json');
+};
+
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`https://ipvigilante.com/json/${ip}`);
+};
+
+const fetchISSFlyOverTimes = function(body){
+  const lat = JSON.parse(body).data.latitude;
+  const lon = JSON.parse(body).data.longitude;
+  return request(`http://api.open-notify.org/iss-pass.json?lat=${lat}&lon=${lon}`);
+}
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+};
+
+module.exports = { nextISSTimesForMyLocation }
